0a48cff418530668ca0ea19451d2b5e6686bbb01
Aysan Isayo <aisayo@icloud.com>
2018-06-20 11:47:57 -0700

    created task form

--- tasks-app/tasks-app-client/src/components/tasks/taskform.js
+++ tasks-app/tasks-app-client/src/components/tasks/taskform.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import Task from './task';
+import React, { Component } from 'react';
 
 
+class TaskForm extends Component {
 
-const TaskForm = () => {
-    return (
-        <div>
-        <form className="TaskForm">
+    constructor(){
+        super();
 
-        </form>
-            <h1>task form</h1>
+        this.state = {
+            task: '',
+            completed: false,
+        }
+    }
+
+    handleOnChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({[name]: value});
+    }
+    
+    render() {
+        return (
+            <div>
+            <h3>Create a New Task</h3>
+            <form className="TaskForm">
+                <label htmlFor="task" className="TaskLabel">Task:</label>
+                <input 
+                    className="TaskInput"
+                    type="text"
+                    name="task"
+                    value={this.state.task}
+                    onChange={(event) => this.handleOnChange(event)}
+
+                />
+
+                < input type="submit" />
+
+            </form>
         </div>
-    )
+        )
+    }
 }
 
 export default TaskForm;
